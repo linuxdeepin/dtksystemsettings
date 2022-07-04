dd81503bd9c08cf8a99c4441667fe75a6ddc24de
guoyao <guoyao@uniontech.com>
2022-07-04 15:18:16 +0800

    feat: add config script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dframework
-Development framework for library. Base on qt5.
+------------
+Development framework based on qt5 for library.
+
+## feature
+1. support cmake and pkg-config module.
+2. support build for debian.
+
+## usage
+1. clone this repostiory.
+2. config your project name:
+```shell
+./config.sh -n name
+```
+3. modify the other config, eg: README.md
---
+++ config.sh
@@ -0,0 +1,59 @@
+#/usr/bin/sh
+# -n name
+# -h help
+
+ChangeName() {
+    echo "begin config name: $1..."
+    name=$1
+    # modify CMakeLists.txt
+    sed -i "s/dframework/${name}/g" CMakeLists.txt
+    # rename config file
+    mv misc/dframework.pc.in "misc/${name}.pc.in"
+    mv misc/dframeworkConfig.cmake.in "misc/${name}Config.cmake.in"
+    # rename debian file
+    mv debian/libdframework.install "debian/lib${name}.install"
+    mv debian/libdframework-dev.install "debian/lib${name}-dev.install"
+    # modify debian file content
+    sed -i "s/dframework/${name}/g" debian/{control,changelog,copyright}
+    echo "end config name"
+}
+
+Print_help() {
+    cat << EOF
+config.sh <options> value
+
+config the project for custom.
+
+eg:./config.sh -n Test
+
+Options:
+  -n project name
+  -h print help
+EOF
+}
+
+while getopts ":n:a:h" optname
+do
+    case "$optname" in
+      "n")
+        ChangeName $OPTARG
+        ;;
+      "h")
+        Print_help
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        ;;
+    esac
+    #echo "option index is $OPTIND"
+done
+
+if [ $# -eq 0 ];then
+    Print_help
+fi
