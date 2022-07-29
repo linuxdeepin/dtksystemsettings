fe2877079373c825d2e3a4e282829b5acf4495f0
guoyao <guoyao@uniontech.com>
2022-07-29 15:24:05 +0800

    chore: add tests

---
+++ tests/ut-demo.cpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include "gtest/gtest.h"
+
+#include "include/demo.h"
+
+DEMO_USE_NAMESPACE
+
+class Tst_Demo : public testing::Test
+{
+public:
+    void SetUp() override
+    {
+        m_demo = new Demo;
+    }
+    void TearDown() override
+    {
+        delete m_demo;
+        m_demo = nullptr;
+    }
+public:
+    Demo *m_demo = nullptr;
+};
+
+TEST_F(Tst_Demo, add)
+{
+    EXPECT_EQ(3, m_demo->add(1, 2));
+}
