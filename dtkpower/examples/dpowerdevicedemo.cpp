d6e72788b018cb9e71ebc9efa8e80c90b88226d3
xiangzelong <xiangzelong@uniontech.com>
2022-10-09 17:42:41 +0800

    chore(dtkpower): examples update
    更新示例
    
    Log: 更新示例

--- dtkpower/examples/dpowerdevicedemo.cpp
+++ dtkpower/examples/dpowerdevicedemo.cpp
@@ -8,9 +8,26 @@
 
 int main(int argc, char **argv)
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
     Dtk::Power::DPowerManager manager;
     auto device = manager.displayDevice();
     qDebug() << device->energyFullDesign();
-    return 0;
+
+    QObject::connect(device.data(), &Dtk::Power::DPowerDevice::updateTimeChanged, &app, [=](const QDateTime &value) {
+        qDebug() << "updateTime:" << value;
+    });
+    QObject::connect(device.data(), &Dtk::Power::DPowerDevice::timeToEmptyChanged, &app, [=](const qint64 value) {
+        qDebug() << "Time:" << value;
+    });
+    QObject::connect(device.data(), &Dtk::Power::DPowerDevice::percentageChanged, &app, [=](const double value) {
+        qDebug() << "percentage:" << value;
+    });
+    QObject::connect(
+        &manager, &Dtk::Power::DPowerManager::lidIsClosedChanged, &app, [=](const bool value) { qDebug() << "Lid:" << value; });
+    QObject::connect(
+        &manager, &Dtk::Power::DPowerManager::deviceAdded, &app, [=](const QString &name) { qDebug() << "name:" << name; });
+    QObject::connect(
+        &manager, &Dtk::Power::DPowerManager::deviceRemoved, &app, [=](const QString &name) { qDebug() << "nameRM:" << name; });
+
+    return app.exec();
 }
