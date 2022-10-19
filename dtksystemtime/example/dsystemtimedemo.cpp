91cba79896783ca7132181377f41c4dd15ce2fa8
xiangzelong <xiangzelong@uniontech.com>
2022-10-19 14:59:21 +0800

    chore(dsystemtime): add example
    添加示例
    
    Log: 添加示例

---
+++ dtksystemtime/example/dsystemtimedemo.cpp
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2022 Uniontech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "dsystemtime.h"
+#include <QCoreApplication>
+#include <iostream>
+#include <qdebug.h>
+#include <qobject.h>
+#include <qeventloop.h>
+DSYSTEMTIME_USE_NAMESPACE
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+    Dtk::SystemTime::DSystemTime systemTime;
+    qDebug() << systemTime.serverName();
+    qDebug() << systemTime.fallbackNTPServers()[1];
+    qDebug() << systemTime.systemNTPServers().isEmpty();
+    qDebug() << systemTime.canNTP();
+}
