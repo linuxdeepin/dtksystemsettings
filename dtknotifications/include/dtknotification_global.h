b3e51c0a51808c65b5d1b0d23ad1a7f8460e3304
wang fei <wangfeia@uniontech.com>
2022-11-08 17:24:50 +0800

    chore: 系统通知和消息
    1. 系统通知和消息的接口；
    2. 系统通知和消息的demo;
    3. 系统通知和消息的单元测试；
    4. 系统通知和消息文档
    
    Log: 系统通知和消息
    Influence: none

---
+++ dtknotifications/include/dtknotification_global.h
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2022 Uniontech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#define DNOTIFICATION_NAMESPACE Dtk::Notifications
+
+#define DNOTIFICATION_USE_NAMESPACE using namespace DNOTIFICATION_NAMESPACE;
+
+#define DNOTIFICATION_BEGIN_NAMESPACE namespace Dtk { namespace Notifications {
+
+#define DNOTIFICATION_END_NAMESPACE }}
