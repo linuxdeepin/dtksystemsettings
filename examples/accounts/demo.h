4d81d115e4f7d0fbdec6c559b5a2860ac8966f63
wangyixue <wangyixue@uniontech.com>
2022-12-09 19:04:06 +0800

    refactor!: change subprojects to modules
    BEARKING_CHANGE: Change subprojects framework to modules framework. Add
    more modern style cmake.
    
    Log: chang subprojects to modules

---
+++ examples/accounts/demo.h
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include "daccountsmanager.h"
+#include "daccountsuser.h"
+#include "daccountstypes.h"
+#include <QObject>
+
+DACCOUNTS_USE_NAMESPACE
+
+class AccountsDemo : public QObject
+{
+    Q_OBJECT
+public:
+    AccountsDemo();
+    void run();
+
+private:
+    DAccountsManager manager;
+    QSharedPointer<DAccountsUser> user{nullptr};
+};
---
+++ examples/login/main.cpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include <QCoreApplication>
+#include "demo.h"
+#include "signaldemo.h"
+#include "propertydemo.h"
+#include "powerdemo.h"
+
+DLOGIN_USE_NAMESPACE
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    QList<LoginDemo *> demos = {new SignalDemo(&app), new PropertyDemo(&app), new PowerDemo(&app)};
+    foreach (const auto demo, demos) {
+        demo->run();
+    }
+    return app.exec();
+}
